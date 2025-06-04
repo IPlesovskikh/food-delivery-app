01a1e204ff6a4091f53f8cf718e1330bd13e79f4
Dmitriy <deims99@mail.ru>
2025-06-04 18:01:57 +0300

    fixed restaurant card and tabs

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -4,14 +4,19 @@ import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
 
+
+
 import { Restaurant, SearchParams } from '@/app/types/types';
 import { useFavoritesStore } from '@/store/favoritesStore';
 import { useNavigationStore } from '@/store/navigationStore';
 import { roboto } from '@/ui/fonts';
 import { fetchAllFavorites, fetchFavorites } from '@/utils/data';
 
+
+
 import Card from './Card';
 import Pagination from './Pagination';
+
 
 type Props = {
   restaurantData: { data: Restaurant[]; totalRestaurants: number };
@@ -128,89 +133,89 @@ export default function RestaurantList({
 
   return (
     <>
-        <TabGroup selectedIndex={selectedTab} onChange={setSelectedTab}>
-          <TabList className="m-auto mx-4 mb-5 flex max-w-[1440px] items-center justify-center lg:mx-auto lg:max-w-[900px] xl:max-w-[1180px]">
-            <Tab className="grow border-b pr-3 pb-1 pl-2 font-semibold text-gray-600 focus:not-data-focus:outline-none data-focus:text-blue-500 data-focus:outline-none data-focus-visible:outline-none data-selected:border-b-2 data-selected:text-blue-600">
-              Все
-            </Tab>
+      <TabGroup selectedIndex={selectedTab} onChange={setSelectedTab}>
+        <TabList className="m-auto mx-4 mb-5 flex max-w-[1440px] items-center justify-center lg:mx-auto lg:max-w-[900px] xl:max-w-[1180px]">
+          <Tab className="min-h-[53px] grow border-b pr-3 pb-1 pl-2 font-semibold text-gray-600 focus:not-data-focus:outline-none data-focus:text-blue-500 data-focus:outline-none data-focus-visible:outline-none data-selected:border-b-2 data-selected:text-blue-600">
+            Все
+          </Tab>
 
-            <Tab className="grow border-b pr-2 pb-1 pl-3 font-semibold text-gray-600 focus:not-data-focus:outline-none data-focus:outline-none data-focus-visible:outline-none data-selected:border-b-2 data-selected:text-blue-600">
-              Недавно заказывали
-            </Tab>
+          <Tab className="min-h-[53px] grow border-b pr-2 pb-1 pl-3 font-semibold text-gray-600 focus:not-data-focus:outline-none data-focus:outline-none data-focus-visible:outline-none data-selected:border-b-2 data-selected:text-blue-600">
+            Недавно заказывали
+          </Tab>
 
-            <Tab className="grow border-b pr-2 pb-1 pl-3 font-semibold text-gray-600 focus:not-data-focus:outline-none data-focus:outline-none data-focus-visible:outline-none data-selected:border-b-2 data-selected:text-blue-600">
-              Избранные
-            </Tab>
-          </TabList>
+          <Tab className="min-h-[53px] grow border-b pr-2 pb-1 pl-3 font-semibold text-gray-600 focus:not-data-focus:outline-none data-focus:outline-none data-focus-visible:outline-none data-selected:border-b-2 data-selected:text-blue-600">
+            Избранные
+          </Tab>
+        </TabList>
 
-          <TabPanels className="m-auto max-w-[1440px] lg:m-auto lg:max-w-[900px] xl:max-w-[1180px]">
-            <TabPanel className="">
-              {restaurants.length > 0 ? (
-                <>
-                  {renderCards(restaurants)}
+        <TabPanels className="m-auto max-w-[1440px] lg:m-auto lg:max-w-[900px] xl:max-w-[1180px]">
+          <TabPanel className="">
+            {restaurants.length > 0 ? (
+              <>
+                {renderCards(restaurants)}
 
-                  {totalRestaurantPages > 1 ? (
-                    <div className="pb-6">
-                      <Pagination
-                        totalPages={totalRestaurantPages}
-                        currentPage={currentRestaurantPage}
-                        paginationName="pageRestaurant"
-                      />
-                    </div>
-                  ) : null}
-                </>
-              ) : (
-                <h2 className={`${roboto.className} mb-4 pt-6 text-2xl font-bold text-gray-800`}>
-                  Ресторанов не найдено
-                </h2>
-              )}
-            </TabPanel>
+                {totalRestaurantPages > 1 ? (
+                  <div className="pb-6">
+                    <Pagination
+                      totalPages={totalRestaurantPages}
+                      currentPage={currentRestaurantPage}
+                      paginationName="pageRestaurant"
+                    />
+                  </div>
+                ) : null}
+              </>
+            ) : (
+              <h2 className={`${roboto.className} mb-4 pt-6 text-2xl font-bold text-gray-800`}>
+                Ресторанов не найдено
+              </h2>
+            )}
+          </TabPanel>
 
-            <TabPanel>
-              {ordersList.length > 0 ? (
-                <>
-                  {renderCards(ordersList)}
+          <TabPanel>
+            {ordersList.length > 0 ? (
+              <>
+                {renderCards(ordersList)}
 
-                  {totalOrdersPages > 1 ? (
-                    <div className="pb-10">
-                      <Pagination
-                        totalPages={totalOrdersPages}
-                        currentPage={currentOrderPage}
-                        paginationName="pageOrder"
-                      />
-                    </div>
-                  ) : null}
-                </>
-              ) : (
-                <h2 className={`${roboto.className} mb-4 pt-6 text-2xl font-bold text-gray-800`}>
-                  Нет недавних заказов
-                </h2>
-              )}
-            </TabPanel>
+                {totalOrdersPages > 1 ? (
+                  <div className="pb-10">
+                    <Pagination
+                      totalPages={totalOrdersPages}
+                      currentPage={currentOrderPage}
+                      paginationName="pageOrder"
+                    />
+                  </div>
+                ) : null}
+              </>
+            ) : (
+              <h2 className={`${roboto.className} mb-4 pt-6 text-2xl font-bold text-gray-800`}>
+                Нет недавних заказов
+              </h2>
+            )}
+          </TabPanel>
 
-            <TabPanel>
-              {favorites.length > 0 ? (
-                <>
-                  {renderCards(favorites)}
+          <TabPanel>
+            {favorites.length > 0 ? (
+              <>
+                {renderCards(favorites)}
 
-                  {totalFavoritesPages > 1 ? (
-                    <div className="pb-10">
-                      <Pagination
-                        totalPages={totalFavoritesPages}
-                        currentPage={currentFavoritePage}
-                        paginationName="pageFavorite"
-                      />
-                    </div>
-                  ) : null}
-                </>
-              ) : (
-                <h2 className={`${roboto.className} mb-4 pt-6 text-2xl font-bold text-gray-800`}>
-                  Избранных ресторанов пока нет
-                </h2>
-              )}
-            </TabPanel>
-          </TabPanels>
-        </TabGroup>
+                {totalFavoritesPages > 1 ? (
+                  <div className="pb-10">
+                    <Pagination
+                      totalPages={totalFavoritesPages}
+                      currentPage={currentFavoritePage}
+                      paginationName="pageFavorite"
+                    />
+                  </div>
+                ) : null}
+              </>
+            ) : (
+              <h2 className={`${roboto.className} mb-4 pt-6 text-2xl font-bold text-gray-800`}>
+                Избранных ресторанов пока нет
+              </h2>
+            )}
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
     </>
   );
 }
